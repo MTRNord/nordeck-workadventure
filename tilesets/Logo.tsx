2d9157ed4393b87b4ef1320b4dc2f7baa14c2ea2
MTRNord <mtrnord1@gmail.com>
2024-03-27 16:55:45 +0100

    Cleanup

--- tilesets/Logo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Logo" tilewidth="32" tileheight="32" tilecount="77" columns="11">
- <image source="Nordeck_Logo_948x648.webp" width="372" height="255"/>
-</tileset>
